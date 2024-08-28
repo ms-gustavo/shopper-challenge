3538f1911e10bc9ce39fd601590f4716f3daa2b2
Gustavo Maia <ms.gustavo@outlook.com>
2024-08-28 11:11:42 -0300

    upload req validators

---
+++ shopper-challenge-backend/src/middlewares/validateUploadRequest.ts
@@ -0,0 +1,27 @@
+import {
+  isValidBase64,
+  isValidDateTime,
+  isValidMeasureType,
+} from "../utils/validators";
+
+export const validateUploadRequest = (body: any) => {
+  const { image, customer_code, measure_datetime, measure_type } = body;
+
+  if (!image || !isValidBase64(image)) {
+    throw new Error("Imagem inválida");
+  }
+
+  if (!customer_code || typeof customer_code !== "string") {
+    throw new Error("Código do cliente inválido");
+  }
+
+  if (!measure_datetime || !isValidDateTime(measure_datetime)) {
+    throw new Error("Data inválida");
+  }
+
+  if (!measure_type || !isValidMeasureType(measure_type)) {
+    throw new Error("Tipo de medida inválido");
+  }
+
+  return body;
+};
