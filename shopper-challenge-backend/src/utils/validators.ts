dae62387c625aa1222d0c4f04d9cc1b1f9dbe14f
Gustavo Maia <ms.gustavo@outlook.com>
2024-08-28 11:00:32 -0300

    initial validators

---
+++ shopper-challenge-backend/src/utils/validators.ts
@@ -0,0 +1,24 @@
+export const isValidBase64 = (base64: string): boolean => {
+  try {
+    if (base64 === "" || base64.trim() === "") return false;
+
+    const isBase64 =
+      /^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/;
+    return isBase64.test(base64);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+    return false;
+  }
+};
+
+export const isValidDateTime = (dateTimeString: string): boolean => {
+  const dateTime = new Date(dateTimeString);
+  return !isNaN(dateTime.getTime());
+};
+
+export const isValidMeasureType = (measureType: string): boolean => {
+  const validTypes = ["WATER", "GAS"];
+  return validTypes.includes(measureType);
+};
